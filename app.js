47bd00416a57bbbecf35d3b1476015b7ad221dbd
staticbubblegum <staticbubblegum@gmail.com>
2024-06-26 21:21:08 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('#mobile_menu');
+const menuLinks = document.querySelector('.navbar_menu');
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is_active');
+    menuLinks.classList.toggle('active');
+});
